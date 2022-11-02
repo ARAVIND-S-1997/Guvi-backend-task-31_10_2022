f74fb7069ab2256a9529ede1d6312c1fd70c79fa
Aravind <itsmearavind4@gmail.com>
2022-11-02 12:16:21 +0530

    refactor: added email check in the email send api

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -1,17 +1,25 @@
+import { findUserService } from "../services/common.service.js";
 import { emailSender } from "../utils/mailSender.js";
 
 export const emailController = {
     sendEmail: async (request, response) => {
         try {
-            const emailInfo = {
-                toaddress: request.body.toaddress,
-                subject: request.body.subject,
-                text: request.body.text,
+            const user = await findUserService(request.body.email);
+            if (user !== null) {
+                const emailInfo = {
+                    toaddress: request.body.toaddress,
+                    subject: request.body.subject,
+                    text: request.body.text,
+                }
+                const emailReq = await emailSender(emailInfo);
+                if (emailReq.messageId) {
+                    response.status(200).json({ message: "Mail has sent successfully" });
+                }
             }
-            const emailReq = await emailSender(emailInfo);
-            if (emailReq.messageId) {
-                response.status(200).json({ message: "Mail has sent successfully" });
+            else {
+                response.status(200).json({ message: "User is not exist please create an account to the send emails " });
             }
+
         } catch (error) {
             response.status(400).json({ message: "something went wrong try again later" });
         }
